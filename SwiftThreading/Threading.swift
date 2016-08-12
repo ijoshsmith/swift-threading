45a2ff72e4ac69d9ae51d70aff3a0cbd18f79583
Meinhard Gredig <github@flexcoding.de>
2016-08-12 17:02:46 +0200

    Swift 3 migration

--- SwiftThreading/Threading.swift
+++ SwiftThreading/Threading.swift
@@ -23,13 +23,13 @@ func ~> <R> (
     backgroundClosure: () -> R,
     mainClosure:       (result: R) -> ())
 {
-    dispatch_async(queue) {
+    DispatchQueue.global().async {
         let result = backgroundClosure()
-        dispatch_async(dispatch_get_main_queue(), {
+        DispatchQueue.main.async {
             mainClosure(result: result)
-        })
+        }
     }
 }
 
 /** Serial dispatch queue used by the ~> operator. */
-private let queue = dispatch_queue_create("serial-worker", DISPATCH_QUEUE_SERIAL)
+private let queue = DispatchQueue(label: "serial-worker")
