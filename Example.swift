ec26bbf61c5efefc5ad50f44e72bd7374ed266ac
Philip Bernstein <ozetadev@me.com>
2016-11-17 19:24:20 -0500

    Example, Swift 3 update

---
+++ Example.swift
@@ -0,0 +1,24 @@
+//
+//  Example.swift
+//  videoRenderer
+//
+//  Created by Philip Bernstein on 11/17/16.
+//  Copyright © 2016 Philip Bernstein. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class Example : NSObject {
+    var i = 1
+
+    func doSomethingCrazyAndUpdateUI() {
+        {
+            while(self.i < 10000) {
+                self.i = self.i + 1
+            }
+        } ~> {
+            let label: UILabel = UILabel()
+            label.text = NSString.localizedStringWithFormat("%d", self.i) as String;
+        }
+    }
+}
